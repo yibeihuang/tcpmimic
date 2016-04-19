185e2cf9707485d868f97f6b5c0243f10301e9e1
Yibei Huang <yh2791@columbia.edu>
2016-04-19 09:00:11 -0400

    modified

--- Receiver.py
+++ Receiver.py
@@ -4,10 +4,13 @@ __author__ = 'yibeihuang'
 import sys
 import socket
 import struct
+import time
+from time import strftime
 
 #-------------------------Classes----------------------
 class receiver:
     def __init__(self, argv):
+        #receiver <filename> <listening_port> <sender_IP> <sender_port> <log_filename>
         filename, listening_port, sender_ip, sender_port, log_filename = argv
         self.log_filename = log_filename
         self.local_port = int(listening_port)
@@ -16,47 +19,73 @@ class receiver:
         self.send_seq = 0
         ############################Receiving###########################
         self.recvsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.recvsock.bind((sender_ip, int(listening_port)))
+        self.recvsock.bind(('', int(listening_port)))
 
         ############################Sending#############################     
         self.sendsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sendsock.connect((sender_ip, self.remote_port))
         ############################Savd File###########################             
         try:
-            self.file_ptr = open(filename, 'w')
+            self.file_ptr = open(filename, 'wb')
         except:
             pass
         ############################Log File############################
+        if argv[4] == 'stdout':
+            self.log_ptr = sys.stdout
+        else:
+            try:
+                self.log_ptr = open(argv[4], 'w')
+            except:
+                print 'unable to write log file'
           
 
     def write_file(self, data):
         self.file_ptr.write(data)
 
-    def write_log(self):
-        pass
+    def write_log(self, header, direction, status):
+        timestamp = strftime("%d,%b,%Y %H:%M:%S", time.localtime())
+        log = str(timestamp) + ' '+ str(header['src_port'])+' '+ str(header['dst_port'])+' '+ str(header['seq']) +' '+ str(header['ack'])+' ' + \
+                str(header['flags']) + ' ' + direction + ' '+ status + '\n'
+        # with open(self.log_filename,'ab') as thefile:
+        #     thefile.write(log)
+        self.log_ptr.write(log)
 
     def send_ack(self, fin):
         header = self.gen_header(fin)
         self.sendsock.send(header)
+        direction = 'send'
+        status = 'ACK'
+        self.write_log(self.parse_header(header), direction, status)
 
-    def get_chksum(self, pkt):
+    def get_chksum(self, header, data):
+        if data:
+            pkt = header + data
+        else:
+            pkt = header
         csvalue = 0
-        for i in  range(0, len(pkt), 2):
-            w = (ord(pkt[i]) << 8) + (ord(pkt[i+1]))
-            csvalue = csvalue + w
-        csvalue = (csvalue>>16) + (csvalue & 0xffff)
+        for i in range(0, len(pkt)):
+            csvalue = csvalue + ord(pkt[i])
         #complement and mask to 4 byte short
         csvalue = ~csvalue & 0xffff
         return csvalue
 
     def check_chksum(self, pkt, chksum):
-        return True         #not implemented yet
+        header = pkt[:20]
+        reheader = struct.unpack('HHIIBBHHH', header)
+        reheader = struct.pack('HHIIBBHHH', reheader[0],reheader[1],reheader[2],reheader[3],reheader[4],reheader[5],reheader[6],0,reheader[8])
+        #repkt = reheader + pkt[20:]
+        rcvcsum = self.get_chksum(reheader, pkt[20:])
+        if chksum == rcvcsum:
+            return True
+        else:
+            return False
 
     def gen_header(self, fin):
         header = struct.pack('HHIIBBHHH',self.local_port, self.remote_port, self.send_seq, self.recv_seq + 1, 0, fin, 0, 0, 0)
-        chksum = self.get_chksum(header)
+        chksum = self.get_chksum(header, None)
         return struct.pack('HHIIBBHHH', self.local_port, self.remote_port, self.send_seq, self.recv_seq + 1, 0, fin, 0, chksum, 0)
         self.send_seq += 1
+
     def parse_header(self, header_packed):
         header = struct.unpack('HHIIBBHHH',header_packed)
         header_fields = ['src_port', 'dst_port', 'seq', 'ack', 'offset', 'flags', 'window', 'chksum', 'urg_ptr']
@@ -66,18 +95,31 @@ class receiver:
         while True:
             data, addr = self.recvsock.recvfrom(1024)
             header = self.parse_header(data[:20])
-            if not self.check_chksum(data, header['chksum']):   #bad chksum, drop packet
+            if not self.check_chksum(data, header['chksum']):   
+                #bad chksum, drop packet
+                print 'corrupted pkt, drop'
+                direction = 'receive'
+                status = 'corrupted'
+                self.write_log(header, direction, status)
                 continue
             #right checksum
             if header['seq'] == self.recv_seq:   #the packet we want
+                direction = 'receive'
+                status = 'successfully'
+                self.write_log(header, direction,status)
                 self.write_file(data[20:])
+
                 self.send_ack(header['flags'] & 0x1)
                 self.recv_seq += 1
             if header['flags'] & 0x1 == 1:   #FIN
+                direction = 'receive'
+                status = 'finalize'
+                self.write_log(header, direction,status)
                 self.file_ptr.close()
                 break
 
 def main(argv):
+    print argv
     receive_hdlr = receiver(argv)
     receive_hdlr.receive()
 
--- Sender.py
+++ Sender.py
@@ -4,6 +4,7 @@ __author__ = 'yibeihuang'
 import sys
 import socket
 import time
+from time import strftime
 import struct
 import os
 import re
@@ -25,7 +26,9 @@ class sender:
     def __init__(self, argv): #filename, remote_IP, remote_port, ack_port_num, log_filename, window_size):
          #sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size>
         #init message to be send in a list
-
+        self.rtt = 1
+        self.devrtt = 1
+        self.timeout = 1
         self.lock = Lock()
         self.sending_message = []
         self.seg_size = 576
@@ -33,31 +36,55 @@ class sender:
         self.window_size = int(argv[5])
         self.window = []
         self.init_window()
+        self.ACKnum = 0
+        self.send_num = 0
+        self.resd_num = 0
+        self.totalbytes = 0
 
-        #sending
+        #==========sending==========
         self.remote_ip = argv[1]
         self.remote_port = int(argv[2])
-        self.sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        #receive
+        try:
+            self.sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except:
+            self.sendsock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        self.sendsock.bind
+        #==========receive===========
         self.local_port = int(argv[3])
-        self.recvsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:           
+            self.recvsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except:
+            self.recvsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        else:
+            print 'socket unable to establish'
         self.recvsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.recvsock.bind(('', self.local_port))
         self.recvsock.listen(1)
         #loging
-        try:
-            self.logfile_ptr = open(argv[4], 'w')
-        except:
-            pass
+        # try:
+        #     self.logfile_ptr = open(argv[4], 'wb')
+        # except:
+        #     pass
+        if argv[4] == 'stdout':
+            self.log_ptr = sys.stdout
+        else:
+            try:
+                self.log_ptr = open(argv[4], 'w')
+            except:
+                print 'unable to write log file'
 
     def get_chksum(self, header, seq):
-        pkt = header + self.sending_message[seq]
+        # psdheader = struct.unpack('HHIIBBHHH',header)
+        # pkt = struct.pack("HHIIBBHHHs", psdheader[0], psdheader[1], psdheader[2], psdheader[3], psdheader[4], \
+        #     psdheader[5], psdheader[6], psdheader[7], psdheader[8], self.sending_message[seq])
         csvalue = 0
-        for i in  range(0, len(pkt), 2):
-            w = (ord(pkt[i]) << 8) + (ord(pkt[i+1]))
-            csvalue = csvalue + w
-        csvalue = (csvalue>>16) + (csvalue & 0xffff)
+        # for i in range(0, len(pkt), 2):
+        #     w = (ord(pkt[i]) << 8) + (ord(pkt[i+1]))
+        #     csvalue = csvalue + w
+        # csvalue = (csvalue>>16) + (csvalue & 0xffff)
+        pkt = header + self.sending_message[seq]
+        for i in range(0, len(pkt)):
+            csvalue = csvalue + ord(pkt[i])
         #complement and mask to 4 byte short
         csvalue = ~csvalue & 0xffff
         return csvalue
@@ -73,11 +100,17 @@ class sender:
     def send_seg(self, seq):
         pkt = self.gen_seg(seq)
         self.sendsock.sendto(pkt, (self.remote_ip, self.remote_port))
+        self.send_num += 1
+        ####write log######
+        direction = 'send'
+        status = 'packet '+str(seq)
+        header = self.parse_header(pkt[:20])
+        self.write_log(header, direction, status)
 
     def gen_msg(self, filename):
         try:
         #small file, read into mem
-            with open(filename, "r") as f:
+            with open(filename, "rb") as f:
                 while True:
                     seg = f.read(self.seg_size)
                     if seg:
@@ -87,18 +120,15 @@ class sender:
         except:
             print 'File not found'
 
-    def write_log(self, logfile):
+    def write_log(self, header, direction, status):
         'log the headers of all the received and sent packets'
         #timestamp, source, destination, Sequence #, ACK #, the flags and estimated RTT
-        log = str(time.strftime('%X %x %Z')) + ' ' + \
-              str(self.source) + ' ' + \
-              str(self.dest) + ' ' + \
-              str(self.seq) + ' ' + \
-              str(self.ACKnum) + ' ' + \
-              str(self.FIN) + ' ' + \
-              str(EstimatedRTT) + '\n'
-        with open('logfile','a') as thefile:
-            thefile.write(log)
+        timestamp = strftime("%d,%b,%Y %H:%M:%S", time.localtime())
+        log = str(timestamp) + ' '+ str(header['src_port'])+' '+str(header['dst_port'])+' '+str(header['seq'])+' '+str(header['ack'])+' ' + \
+                str(header['flags']) + ' ' + direction + ' ' + status + 'estimated rtt '+str(self.rtt) + '\n'
+        # with open(self.log_file,'ab') as thefile:
+        #     thefile.write(log)
+        self.log_ptr.write(log)
 
     def refresh_rtt(self, sample_rtt):
         self.rtt = (1-0.125)*self.rtt + 0.125*sample_rtt
@@ -106,12 +136,12 @@ class sender:
         self.timeout = self.rtt + 4*self.devrtt
 
     def init_window(self):
-        for x in range(self.window_size):
+        for x in range(self.window_size if self.window_size < len(self.sending_message) else len(self.sending_message)):
             self.window.append(node({
                 'seq':      x,
                 'acked':    False,
                 'sent':     False,
-                'ts':       time(),
+                'ts':       time.time(),
             }))
 
     def parse_header(self, header_packed):
@@ -125,11 +155,14 @@ class sender:
             header = self.parse_header(recv)
             self.lock.acquire()
             for x in range(len(self.window)):
-                if self.window[x].seq == header['ack'] - 1:
+                if self.window[x].seq == header['ack'] - 1:#correctly received an ACK
                     self.window[x].acked = True
-                    print 'ack %d received' % header['ack']
-                    sample_rtt = time() - self.window[x].ts
+                    #print 'ack %d received' % header['ack']
+                    sample_rtt = time.time() - self.window[x].ts
                     self.refresh_rtt(sample_rtt)
+                    direction = 'receive'
+                    status = 'ack '
+                    self.write_log(header, direction, status)
             self.lock.release()
             if header['flags'] & 0x1 == 1:
                 break
@@ -142,19 +175,20 @@ class sender:
         t = Thread(target = self.process_ack, args=(conn,))
         t.start()
 
+
     def send(self):
         self.enable_ack()
         print 'start sending'
         while True:
             #check and shift window
             for x in range(len(self.window)):
-                if not self.window[x].sent:  #node not send,
+                if not self.window[x].sent:  #node not send
                     self.send_seg(self.window[x].seq)
                     self.window[x].sent = True
-                    self.window[x].ts = time()
+                    self.window[x].ts = time.time()
                     continue
 
-                if self.window[x].acked:
+                if self.window[x].acked: #node received correctly
                     self.lock.acquire()
                     for idx in range(x+1):
                         if self.window[-1].seq < len(self.sending_message) - 1:
@@ -162,22 +196,25 @@ class sender:
                                 'seq':      self.window[-1].seq + 1,
                                 'acked':    False,
                                 'sent':     False,
-                                'ts':       time(),
+                                'ts':       time.time(),
                             }))
                         del self.window[0]
                     self.lock.release()
                     break   #window length changed, break for loop and re-enter
 
-                if time()-self.window[x].ts > self.timeout: # timeout retransmit
+                if time.time()-self.window[x].ts > self.timeout: # timeout retransmit
                     self.send_seg(self.window[x].seq)
-                    self.window[x].ts = time()
+                    self.resd_num += 1
+                    self.window[x].ts = time.time()
 
             if not self.window: #all data sent, finish
-                break
+                print 'Delivery completed successfully'
+                print 'Total Bytes sent =' + str()
+                print 'Segments sent = ' + str(self.send_num)
+                print 'Segment retransmitted = %f%'%(self.resd_num/self.send_num)
 
 def main(argv):
-
-     #sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size>
+    #sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size>
     #if len(argv != 6):
     #    print 'argument input wrong, please follow the format as below:\n'
     #    print 'sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size>'
